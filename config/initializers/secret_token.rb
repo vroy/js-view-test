867d87e9e1a804e21b4620ee502fef57512f83e1
Vincent Roy <vincentroy8@gmail.com>
2013-02-22 21:08:05 -0400

    Initial rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ViewJsTest::Application.config.secret_token = 'c90d0bcef936dc48ed3084f2dedcdd4e456cf1e65daebbdec6296435c4ce67b074b7285ee47aa87428e25ec1fb81700549a398487b36db6db130d5c1e5827ebe'
